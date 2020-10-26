c87254f5daa23dbc0b71973de5fc365bad6bf4a5
ethanzh <ethan.houston@gmail.com>
2020-10-26 12:31:11 -0500

    add second layer

--- neural_network.py
+++ neural_network.py
@@ -77,10 +77,13 @@ class Classifier(nn.Module):
 
     def __init__(self, num_classes):
         super(Classifier, self).__init__()
-        self.fc1 = nn.Linear(512, num_classes)
+        self.fc1 = nn.Linear(512, 256)
+        self.fc2 = nn.Linear(256, num_classes)
 
     def forward(self, x):
         x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
         x = F.softmax(x, dim=1)
 
         return x
